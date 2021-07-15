e3bbeaeae150752c4e09b8e261ab0766088fb45e
bhasan7 <76016223+bhasan7@users.noreply.github.com>
2021-07-15 08:49:16 +0200

    Add files via upload

---
+++ Project5/script.js
@@ -0,0 +1,17 @@
+var boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+function checkBoxes() {
+  var triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    var boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
